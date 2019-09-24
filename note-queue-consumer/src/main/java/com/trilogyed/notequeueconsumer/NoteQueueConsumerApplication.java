96a78bae0ff708643c097096dfd797f39657aba1
Blora616 <Blora616@gmail.com>
2019-09-24 08:26:07 -0400

    add config && spring name

--- note-queue-consumer/src/main/java/com/trilogyed/notequeueconsumer/NoteQueueConsumerApplication.java
+++ note-queue-consumer/src/main/java/com/trilogyed/notequeueconsumer/NoteQueueConsumerApplication.java
@@ -7,10 +7,12 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NoteQueueConsumerApplication {
 	public static final String TOPIC_EXCHANGE_NAME = "note-exchange";
